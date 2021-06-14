14573a9d89603b89f9b9c9da594c3bd433a276c0
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-06-14 11:21:17 -0400

    Support whole-url mocking

--- asgiar/__init__.py
+++ asgiar/__init__.py
@@ -1,7 +1,9 @@
 """ASGI AR."""
+from fnmatch import fnmatch
 from functools import partial, wraps
 from typing import Any, List
 from unittest import mock
+from urllib.parse import urlparse
 
 import httpx
 
@@ -21,11 +23,20 @@ class ASGIAR(AsyncContextDecorator):
             self,
             asgi_app,
             host: str = None,
+            url: str = None,
     ) -> None:
         """Initialize."""
         self._patches: List[mock._patch] = []
         self._transport = httpx.ASGITransport(app=asgi_app)
-        self._host: str = host.lower()
+        if host is not None:
+            self._host: str = host.lower()
+        if url is not None:
+            parsed = urlparse(url)
+            self._url_prefix: str = f"{parsed.scheme}://{parsed.netloc}"
+            self._url_path: str = parsed.path
+        else:
+            self._url_prefix = f"http://{self._host}"
+            self._url_path = "/*"
         super().__init__()
 
     def __enter__(self) -> "ASGIAR":
@@ -61,8 +72,12 @@ class ASGIAR(AsyncContextDecorator):
     def _handle_async_request(self, spec):
         async def request(_self, *args, **kwargs):
             pass_through = partial(spec, _self)
-            host = next(header[1].decode() for header in kwargs["headers"] if header[0] == b"Host")
-            if self._host is None or host == self._host:
+            if "url" not in kwargs:
+                return await pass_through(*args, **kwargs)
+            scheme, host, port, path = kwargs["url"]
+            prefix = scheme.decode() + "://" + host.decode() + (":" + port.decode() if port is not None else "")
+            path = path.decode()
+            if prefix == self._url_prefix and fnmatch(path, self._url_path):
                 return await self._transport.handle_async_request(
                     *args,
                     **kwargs,
